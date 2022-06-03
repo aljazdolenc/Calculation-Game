5ad77c04ac3d0b1a1458edd6e87feb918a48b2c9
aljazdolenc5@gmail.com <aljazdolenc5@gmail.com>
2022-06-03 13:57:23 +0200

    Created Material module and moved Mat modules in it

---
+++ calculation-game/src/app/material.module.ts
@@ -0,0 +1,25 @@
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { LayoutModule } from "@angular/cdk/layout";
+import { NgModule } from "@angular/core";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { MatInputModule } from '@angular/material/input/public-api';
+
+
+const MaterialComponents:any[]=[
+  MatFormFieldModule,
+  MatInputModule,
+  MatIconModule,
+  MatListModule,
+  MatGridListModule,
+  LayoutModule
+];
+
+@NgModule({
+  imports:[MaterialComponents],
+  exports:[MaterialComponents],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'fill'}}],
+})
+export class MaterialModule{ }
